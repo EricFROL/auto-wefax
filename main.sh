6dfe31465368fc115f9ceaa592a2412d3e1b3064
ericfrol <42719382+EricFROL@users.noreply.github.com>
2023-01-28 17:43:36 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -51,7 +51,7 @@ do
             echo "11 minuts"
             timeout=660
         fi
-        timeout -k 10 $timeout rtl_fm -f 138881000 -M usb -T -s 48k | sox -r 48k -t raw -e s -b 16 -c 1 - -d | timeout -k 10 $timeout fldigi
+        timeout -k 10 $timeout rtl_fm -f 138881000 -M usb -T -s 44.1k | sox -r 44.1k -t raw -e s -b 16 -c 1 - -d | timeout -k 10 $timeout fldigi
         find /home/pi/.fldigi/images -type f -size +2M -exec mv "{}" /var/www/html/data/wefax-images/ \;
         cname=$(ls -tr /var/www/html/data/wefax-images | tail -n 1)
         ls -tr /var/www/html/data/wefax-images | tail -n 1 | xargs -I{} mv {} "/var/www/html/data/wefax-images/$hora-wefax.png"
