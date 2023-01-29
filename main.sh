a0d9bba1a5dfe7a4074ef316c859a4d6c74af299
ericfrol <42719382+EricFROL@users.noreply.github.com>
2023-01-29 20:29:50 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -55,7 +55,7 @@ do
         find /home/pi/.fldigi/images -type f -size +2M -exec mv "{}" /var/www/html/data/wefax-images/ \;
         cname=$(ls -tr /var/www/html/data/wefax-images | tail -n 1)
         ls -tr /var/www/html/data/wefax-images | tail -n 1 | xargs -I{} mv {} "/var/www/html/data/wefax-images/$hora-wefax.png"
-
+        rtl_biast -b 0
 
     fi
 done
