95d630d9c4bb14dabe487e4df53fb983550983c2
ericfrol <42719382+EricFROL@users.noreply.github.com>
2023-06-01 14:30:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,63 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		</style>
-	</head>
-	<body>
-  <?php 
-  $directory = "./data/wefax-images";
-  $images = glob($directory . "/*.png");
-  
-  foreach($images as $image)
-  {
-    echo "<img src=".$image."> \n  ";
-  }
-  ?>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <title>Galería de Imágenes</title>
+    <style>
+      .container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 20px;
+        margin-top: 50px;
+      }
+
+      .tray {
+        border: 1px solid #000;
+        width: 300px;
+        height: auto;
+        padding: 10px;
+        text-align: center;
+        font-size: 14px;
+
+      }
+
+      .tray-title {
+        font-size: 24px;
+        font-weight: bold;
+        text-decoration: underline;
+      }
+
+      .tray-date {
+        font-size: 20px;
+        font-weight: bold;
+        text-decoration: underline;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <?php
+      $directory = "./";
+      $images = glob($directory . "/*.png");
+      $imageGroups = array();
+
+      foreach ($images as $image) {
+          $date = date("Y/m/d", filemtime($image));
+          $imageGroups[$date][] = $image;
+      }
+
+      krsort($imageGroups);
+
+      foreach ($imageGroups as $date => $imageGroup) {
+          echo '<div class="tray">';
+          echo '<h3>' . $date . '</h3>';
+          foreach ($imageGroup as $image) {
+              echo '<a href="' . $image . '">' . basename($image) . '</a><br>';
+          }
+          echo '</div>';
+      }
+      ?>
+    </div>
+  </body>
 </html>
